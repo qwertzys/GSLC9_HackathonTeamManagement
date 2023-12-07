82b5cc3470d7945b7155d526c9d6679749703cef
qwertzys <142578957+qwertzys@users.noreply.github.com>
2023-12-07 17:32:43 +0700

    Update Connection.java

--- OOPGSLC9/src/Connection.java
+++ OOPGSLC9/src/Connection.java
@@ -11,8 +11,18 @@ import java.io.IOException;
 // c. menulis ke dalam file .csv
 
 public class Connection {
-	
-	String filePathUser = "https://github.com/qwertzys/GSLC9_HackathonTeamManagement/blob/main/OOPGSLC9/src/user.csv";
-	String filePathTeam = "https://github.com/qwertzys/GSLC9_HackathonTeamManagement/blob/main/OOPGSLC9/src/user.csv";
-	
+    private CSVParser csvParser;
+
+    public Connection(String filePath) {
+        try {
+            Reader reader = new FileReader(filePath);
+            this.csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withHeader());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public CSVParser getCsvParser() {
+        return csvParser;
+    }
 }
