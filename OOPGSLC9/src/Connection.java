1766f81bc589d610d869823ea6b395241debff68
qwertzys <142578957+qwertzys@users.noreply.github.com>
2023-12-07 17:12:36 +0700

    Add files via upload

---
+++ OOPGSLC9/src/Connection.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+// Class ini tujuannya adalah:
+// a. membuka file .csv
+// b. membaca file .csv
+// c. menulis ke dalam file .csv
+
+public class Connection {
+	
+	String filePathUser = "C:\\Users\\ASUS\\AppData\\Local\\Temp\\Temp1_Hackathon Team Management GLS CASE STUDY (1).zip\\Database.zip\\user.csv";
+	String filePathTeam = "C:\\Users\\ASUS\\AppData\\Local\\Temp\\Temp1_Hackathon Team Management GLS CASE STUDY (1).zip\\Database.zip\\teams.csv";
+	
+}
---
+++ OOPGSLC9/src/main.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner inp = new Scanner(System.in); // Scanner
+		int choice = 0; // default choice mulai dari 0 untuk pilih options
+		while (choice != 4) { // Loop Menu utama terus sampe kena exit
+			// Menu Utama
+			System.out.println("1. Menu Utama");
+			System.out.println("2. Insert Data");
+			System.out.println("3. Show");
+			System.out.println("4. Exit");
+			System.out.println("Select the following option: ");
+			choice = inp.nextInt(); // scan integer
+			
+			// Menjalankan program berdasarkan pilihan choice
+			switch(choice) {
+				case 1: { // Menampilkan menu utama awal program
+					
+					break;
+				}
+				
+				case 2: { // Insert Data
+					int insertTable = 0;
+					do {
+						System.out.println("Which Table to insert?");
+						System.out.println("1. User");
+						System.out.println("2. Team");
+						insertTable = inp.nextInt();
+						if (insertTable == 1) {
+							System.out.print("Add Name: ");
+							System.out.print("Add NIM: ");
+							System.out.print("Add Team: ");
+						} else if (insertTable == 2) {
+							System.out.print("Add Team Name: ");	
+						} else {
+							System.out.println("Invalid Option, try again");
+						}
+					} while (insertTable != 1 || insertTable != 2);
+					
+					// butuh function untuk insert data kepada csv dibawah line ini
+					
+					break;
+				}
+				
+				case 3: { // Menunjukkan tabel 
+					int showTable = 0;
+					int conditionChoice = 0;
+					String condition;
+					do {
+						System.out.println("Which Table to Show?");
+						System.out.println("1. User");
+						System.out.println("2. Team");
+						showTable = inp.nextInt();
+						
+//						if (showTable == 1) {
+//							
+//						} else if (showTable == 2) {
+//							
+//						} else {
+//							System.out.println("Invalid Option, try again");
+//						}
+						
+						
+						System.out.println("Want to filter by condition?");
+						System.out.println("1. Yes");
+						System.out.println("2. No");
+						conditionChoice = inp.nextInt();
+						if (conditionChoice == 2) {
+							// show semua table
+							// Butuh function untuk menunjukkan semua table
+						} else { // show table sesuai dengan kondisi yang terkasih
+							System.out.println("Add condition, seperate by semicolon [condition;=;value]: ");
+							condition = inp.next();
+							
+							// Butuh function dibawah line ini untuk menunjukkan tabel sesuai dengan kondisi yang terkasih
+						}
+					} while (showTable != 1 || showTable != 2);
+					break;
+				}
+				
+				// default ini ignore aja, lagi testing
+//				default: { // kalau pilih nomor lain
+//					System.out.println("Invalid Option, please try again");
+////					inp.next();
+//					break;
+//				}
+			}
+		}
+		
+		System.out.println("Program Exited");
+	}
+}
