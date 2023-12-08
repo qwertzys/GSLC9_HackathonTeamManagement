b0b1d5d5e5bca4833ec5e172d62c2affc66496b0
stebby70 <153183086+stebby70@users.noreply.github.com>
2023-12-08 11:09:32 +0700

    Update Team.java

--- OOPGSLC9/src/Models/Team.java
+++ OOPGSLC9/src/Models/Team.java
@@ -4,9 +4,14 @@ import Repository.*;
 
 public class Team extends Model implements TeamRepository {
 	String name;
+	int member;
+	String teamFilePath = "";
 	
 	public void setName(String name) {
 		this.name = name;
+	}
+	public String getName() {
+		return name;
 	}
 	public ArrayList<Team> find(){
 		
