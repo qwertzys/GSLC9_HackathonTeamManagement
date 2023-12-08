7480c4860782098a3b7de5cde706e33727c8c53f
stebby70 <153183086+stebby70@users.noreply.github.com>
2023-12-08 11:10:13 +0700

    Update User.java

--- OOPGSLC9/src/Models/User.java
+++ OOPGSLC9/src/Models/User.java
@@ -5,12 +5,20 @@ import Repository.*;
 public class User extends Model implements UserRepository {
 	String name;
 	String nim;
+	String userFilePath = "";
 	
 	public void setName(String name) {
 		this.name = name;
 	}
 	public void setNim(String nim) {
 		this.nim = nim;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String getNim() {
+		return nim;
 	}
 	
 	public ArrayList<User> find(){
